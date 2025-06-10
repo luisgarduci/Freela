18b55084a9bd14b0371cff5a5f028a62ce52fee6
luisgarduci <112954236+luisgarduci@users.noreply.github.com>
2025-06-10 15:57:38 -0300

    changes in some sections and images added and excluded

--- script.js
+++ script.js
@@ -1,7 +1,55 @@
-let currentSlide = 0;
-let burguer_menu = document.getElementById("burguer-menu");
-let phone_menu = document.getElementById("phone-menu")
-const slides = document.querySelectorAll('.carousel-slide');
+let currentBanner = 1;
+const burguer_menu = document.getElementById("burguer-menu");
+const phone_menu = document.getElementById("phone-menu");
+const back_top = document.getElementById("back-top");
+const left_arrow = document.getElementById("left-arrow");
+const right_arrow = document.getElementById("right-arrow");
+const banner = document.getElementById("banner");
+
+
+setInterval(() => {
+   if (currentBanner !== 3) {
+    currentBanner = currentBanner + 1
+   banner.src = `images/banner${currentBanner}.jpg`;
+   }
+   else {
+   currentBanner = 1;
+   banner.src = `images/banner${currentBanner}.jpg`
+   }
+
+}, 9000)
+
+
+left_arrow.addEventListener("click", () => {
+    if (currentBanner !== 1) {
+     currentBanner = currentBanner - 1;
+     banner.src = `images/banner${currentBanner}.jpg`;
+   }
+   else {
+    currentBanner = 3;
+    banner.src = `images/banner${currentBanner}.jpg`;
+   }
+})
+
+right_arrow.addEventListener("click", () => {
+  if (currentBanner !== 3) {
+    currentBanner = currentBanner + 1;
+    banner.src = `images/banner${currentBanner}.jpg`
+  }
+  else {
+    currentBanner = 1;
+    banner.src = `images/banner${currentBanner}.jpg`
+  }
+})
+
+document.addEventListener("scroll", (event) => {
+  back_top.style.display = "block";
+  console.log(event)
+})
+back_top.addEventListener("click", (event) => {
+  back_top.style.display = "none";
+  window.scrollTo(0, 0);
+})
 
 burguer_menu.addEventListener("click", (e) => {
       
@@ -13,24 +61,3 @@ burguer_menu.addEventListener("click", (e) => {
       }
 })
 
-function showSlide(index) {
-  slides.forEach((slide, i) => {
-    slide.classList.remove('active');
-    if (i === index) {
-      slide.classList.add('active');
-    }
-  });
-}
-
-function nextSlide() {
-  currentSlide = (currentSlide + 1) % slides.length;
-  showSlide(currentSlide);
-}
-
-function prevSlide() {
-  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-  showSlide(currentSlide);
-}
-
-// Mostrar o primeiro slide ao carregar
-showSlide(currentSlide);
