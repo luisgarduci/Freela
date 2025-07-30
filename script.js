586b7ceb987088a94e3506e1e3f36b9271299bff
luisgarduci <112954236+luisgarduci@users.noreply.github.com>
2025-07-30 07:28:41 -0300

    changes

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const phone_menu = document.getElementById("phone-menu");
 burguer_menu.addEventListener("click", (e) => {
       
       if (phone_menu.style.visibility === "visible") {
-        phone_menu.style.transition = "0.5s"
+        phone_menu.style.transition = "0.5s";
         phone_menu.style.visibility = "hidden";
       }
       else {
-        phone_menu.style.visibility = "visible"
-        phone_menu.style.transition = "0.5s"
+        phone_menu.style.visibility = "visible";
+        phone_menu.style.transition = "0.5s";
       }
 })
 
